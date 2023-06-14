2f9ad3544ba501108d90375e30709849d7f4a166
Gabi Rosa <31.gabirosa@gmail.com>
2023-06-13 21:23:11 -0300

    Service layer, component registration

--- src/main/java/com/course/spring/boot/coursespring/repositories/UserRepository.java
+++ src/main/java/com/course/spring/boot/coursespring/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.course.spring.boot.coursespring.repositories;
 
 import com.course.spring.boot.coursespring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/course/spring/boot/coursespring/resource/UserResource.java
+++ src/main/java/com/course/spring/boot/coursespring/resource/UserResource.java
@@ -1,18 +1,33 @@
 package com.course.spring.boot.coursespring.resource;
 
 import com.course.spring.boot.coursespring.entity.User;
+import com.course.spring.boot.coursespring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "95445538", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+    @GetMapping(value = "/{id}")
+        public ResponseEntity<User> findById(@PathVariable Long id) {
+            User obj = service.findById(id);
+            return ResponseEntity.ok(obj);
+
+    }
+
 }
